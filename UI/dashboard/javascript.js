f7d6d299d172c03d877eeaaf85cea71d8c9a8b90
T2003 <tuyizere925@gmail.com>
2022-05-11 11:04:44 +0200

    add

--- UI/dashboard/javascript.js
+++ UI/dashboard/javascript.js
@@ -7,3 +7,39 @@ sidebarBtn.onclick = function() {
 }else
   sidebarBtn.classList.replace("bx-menu-alt-right", "bx-menu");
 }
+
+let isOn=false;
+const showMenu=()=>{
+    const div=document.querySelectorAll(".mobile-menu");
+isOn?div[0].style.display="none": div[0].style.display="flex";
+isOn=!isOn;
+}
+document.getElementById("showMenu").addEventListener('click', showMenu);
+
+let isUserProfileOn=false;
+const showUserProfile=()=>{
+    const div=document.querySelectorAll(".user-profile");
+    isUserProfileOn?div[0].style.display="none": div[0].style.display="block";
+isUserProfileOn=!isUserProfileOn;
+}
+document.getElementById("userDiv").addEventListener('click', showUserProfile);
+
+///////////////////////////////////
+
+const isAdminLogedIn=()=>{
+    const loginedUser = JSON.parse(localStorage.getItem("loginedUser")) || [];
+    if (loginedUser.role != "admin") {
+      location.href='../login.html'
+    }
+}
+isAdminLogedIn()
+
+const logout = () => {
+    localStorage.removeItem("loginedUser");
+    location.href='../index.html'
+  };
+
+document.getElementById("logoutBtn").addEventListener('click',logout)
+
+
+

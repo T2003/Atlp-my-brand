8be13570aa405e74bbe1af1099ce4a65d39bebc9
T2003 <tuyizere925@gmail.com>
2022-05-17 12:48:09 +0200

    update UI

--- UI/dashboard/javascript.js
+++ UI/dashboard/javascript.js
@@ -1,28 +1,3 @@
-let sidebar = document.querySelector(".sidebar");
-let sidebarBtn = document.querySelector(".sidebarBtn");
-sidebarBtn.onclick = function() {
-  sidebar.classList.toggle("active");
-  if(sidebar.classList.contains("active")){
-  sidebarBtn.classList.replace("bx-menu" ,"bx-menu-alt-right");
-}else
-  sidebarBtn.classList.replace("bx-menu-alt-right", "bx-menu");
-}
-
-let isOn=false;
-const showMenu=()=>{
-    const div=document.querySelectorAll(".mobile-menu");
-isOn?div[0].style.display="none": div[0].style.display="flex";
-isOn=!isOn;
-}
-document.getElementById("showMenu").addEventListener('click', showMenu);
-
-let isUserProfileOn=false;
-const showUserProfile=()=>{
-    const div=document.querySelectorAll(".user-profile");
-    isUserProfileOn?div[0].style.display="none": div[0].style.display="block";
-isUserProfileOn=!isUserProfileOn;
-}
-document.getElementById("userDiv").addEventListener('click', showUserProfile);
 
 ///////////////////////////////////
 
